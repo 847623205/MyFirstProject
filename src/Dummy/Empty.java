29e398a21cc1c4198104e87e121fe1d3f20adb2f
847623205 <51754373+847623205@users.noreply.github.com>
2019-06-12 22:34:30 +0530

    Update Empty.java

--- src/Dummy/Empty.java
+++ src/Dummy/Empty.java
@@ -5,6 +5,7 @@ public class Empty {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hai Welcome");
+		System.out.println("Hai Welcome You all");
 
 	}
 
