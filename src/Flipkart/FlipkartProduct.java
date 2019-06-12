a89914815a660b17f4f4b74692674b7a373e4e05
unknown <ersarathy1991@gmail.com>
2019-06-12 21:45:41 +0530

    My first commit

---
+++ src/Flipkart/FlipkartProduct.java
@@ -0,0 +1,40 @@
+package Flipkart;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverService;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class FlipkartProduct 
+{
+	
+WebDriver driver;
+	
+	@BeforeTest
+	public void Configuration()
+	{
+		System.setProperty("webdriver.gecko.driver", "D:\\Backup\\Eclipse\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.get("https://www.flipkart.com");
+		
+	}
+	
+	
+	@Test
+	public void ExcelWrite()
+	{
+		System.out.println("Hi");
+	}
+	
+	
+	@AfterTest
+	public void ExitPoint()
+	{
+		
+	}
+
+}
---
+++ src/Sathya/SathyaProduct.java
@@ -0,0 +1,33 @@
+package Sathya;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class SathyaProduct 
+{
+	WebDriver driver;
+	
+	@BeforeTest
+	public void Configuration()
+	{
+		
+	}
+	
+	
+	@Test
+	public void ExcelWrite()
+	{
+		
+	}
+	
+	
+	@AfterTest
+	public void ExitPoint()
+	{
+		
+	}
+	
+
+}
